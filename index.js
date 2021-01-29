6b42f9f63bb2a884045f7b27a2ce4a71c9537979
osagie_dot_developer <adesuwa.osagie@aol.com>
2021-01-29 23:13:29 +0000

    Solution with assert function

--- index.js
+++ index.js
@@ -13,3 +13,44 @@
 //   dislikes: 3,
 //   followers: 10
 // }) ➞ [["likes", 2], ["dislikes", 3], ["followers", 10]]
+
+function arrayToObjects (object) {
+  let result = [];
+  let subArray = [];
+  //iterate over the properties in the input object
+  for (let key in object) {
+    //push key to subArray
+    subArray.push(key);
+    //push value to subArray
+    subArray.push(object[key]);
+    //push subArray to result
+    result.push(subArray);
+    //reset subArray
+    subArray = [];
+  }
+
+  return result;
+}
+
+function assertObjectEquals(actual, expected, testName) {
+  actual = JSON.stringify(actual);
+  expected = JSON.stringify(expected);
+
+  if (actual === expected) {
+    console.log ('test passed')
+  } else {
+    console.log(`FAILED [${testName}] Expected ${expected}, but got ${actual}`)
+  }
+
+}
+
+let data = {
+  D: 1,
+  B: 2,
+  C: 3
+}
+
+let actual = arrayToObjects(data);
+let expected = [["D", 1], ["B", 2], ["C", 3]];
+
+console.log ( assertObjectEquals(actual, expected, 'object to array') )
